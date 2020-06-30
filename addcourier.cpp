b4fd1a8d80138932ccbb57f53ae40134db95f1f2
rozybaeva6 <61972638+rozybaeva6@users.noreply.github.com>
2020-06-30 22:29:09 +0300

    Update addcourier.cpp

--- addcourier.cpp
+++ addcourier.cpp
@@ -3,77 +3,84 @@
 #include <fstream>
 #include <string>
 #include "classes.h"
-
 using namespace std;
-
 void addcourier (string courierfile)
-{//Добавление курьеров
+{
 	ifstream fin;
 	ofstream fout;
 	fin.open(courierfile);	
 	int  i=0;
 	int k=0;
 	string s;
-	cout << "Enter the courier you want to put('name /n phone /n car(0 or 1)')"<<endl;
-	while (!fin.eof()){//Считаем количество строк в тексте=количество видов объектов
+	cout << "Enter the courier you want to put('name /n phone  /n car(0 or 1)')"<<endl;
+	while (!fin.eof()){
 		getline(fin, s);
 		k++;
 	}
 	k--;
-	Courier *a = new Courier[k+1];//Выделяем память на массив типа  struct(с запасом)
-	//fin.seekg( 0 );//Не забываем вернуть курсор в начало
-	fin.close();//Убираем за собой
+	int sidc;	
+	string sname;
+	string sphone;
+	int sorderid1;
+	int sorderid2;
+	int scar; 
+	int sdeliverytime;		
+	Courier *a = new Courier[k+1];
+	fin.close();
 	fin.open(courierfile);	
 	for (i=0; i<k; i++){
-		fin >> a[i].idc;
-		fin >> a[i].name ;
-		fin >> a[i].phone;
-		fin >> a[i].orderid1;
-		fin >> a[i].orderid2
-		fin >> a[i].car;
-		fin >> a[i].deliverytime;
-		//Считываем элементы из файла, заодно выводим на экран все текущие элементы
+		fin >> sidc;
+		fin >> sname ;
+		fin >> sphone;
+		fin >> sorderid1;
+		fin >> sorderid2;
+		fin >> scar;
+		fin >> sdeliverytime;
+		a[i].setidc(sidc);
+		a[i].setname(sname);
+		a[i].setphone(sphone);
+		a[i].setorderid1(sorderid1);
+		a[i].setorderid2(sorderid2);
+		a[i].setcar(scar);
+		a[i].setdeliverytime(sdeliverytime);
 	}
 	int j;
-	string name;
-	string phone;
-	int idc;
-	int car;		
-	cin >> name >> phone >> car; 
-	idc=k+1;
+	cin >>sname>>sphone>>scar;
+	sidc=k+1;
 	int f=0;
 	while(f==0){
 		f=1;
 		for (i=0; i<k; i++){
-			if (a[i].idc== idc){ 
+			if (a[i].getidc()== sidc){ 
 				f=0;
-				idc=idc+1;
-				fin.close();	
+				sidc=sidc+1;	
 			}
-			if (a[i].name== name){ // сравниваем параметры
+			if (a[i].getname()== sname){ 
 				cout <<"courier with same name already exists. Try the other one"<<endl;
 				fin.close();
 				exit(0);	
 			}
-			if (a[i].phone== phone){ // сравниваем параметры
+			if (a[i].getphone()== sphone){ 
 				cout <<"courier with same phone already exists. Try the other one"<<endl;
 				fin.close();
 				exit(0);	
 			}
 		}
 	}
-	fin.close();//Готовимся полностью очистить файл
-	a[k].idc=idc;
-	a[k].name=name;
-	a[k].phone=phone;
-	a[k].car=car;
-	a[k].deliverytime=0;
-	a[k].orderid1=0;
-	a[k].orderid2=0;
-	fout.open(courierfile, ios::out);//Мы снова в файле, но он теперь пустой
-	for (i=0; i<k+1; i++){//Переписываем весь дополненный массив в документ
-		fout << a[i].idc <<" "<<a[i].name<<" "<<a[i].phone<<" "<<a[i].orderid1<<" "a[i].orderid2<<" "<<a[i].car<<" "<<a[i].deliverytime<<endl;
+	fin.close();
+	a[k].setidc(sidc);
+	a[k].setname(sname);
+	a[k].setphone(sphone);
+	a[k].setorderid1(0);
+	a[k].setorderid2(0);
+	a[k].setcar(scar);
+	a[k].setdeliverytime(30-scar*20);
+	fout.open(courierfile, ios::out);
+	for (i=0; i<k+1; i++){
+		fout << a[i].getidc() <<" "<<a[i].getname()<<" "<<a[i].getphone()<<" "<<a[i].getorderid1()<<" "<<a[i].getorderid2()<<" "<<a[i].getcar()<<" "<<a[i].getdeliverytime()<<endl;
 	}
-	fout.close();//Убираем за собой
-	delete [] a;//Убираем за собойх2
+	fout.close();
+	delete [] a;
 }
+
+
