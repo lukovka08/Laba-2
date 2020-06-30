01c3b5abb14dda205ab47522dd8c22525901b234
IshuDivan <62930631+IshuDivan@users.noreply.github.com>
2020-06-30 22:21:01 +0300

    Update oselect.cpp
    omg, that's a lot

--- oselect.cpp
+++ oselect.cpp
@@ -20,15 +20,27 @@ void oselect(string courierfile, string orderfile)
 	Order *a = new Order[k+1];//массив заказов
 	fin.close();
 	fin.open(orderfile);
+	int sido;
+        int sprice;
+        int sdeadline;
+        string saddress;
+        int scourierid;
+        int sdtime;
 	int nul=0;
 	for (i=0; i<k; i++){
-		fin >> a[i].ido;//заносим заказы в массив
-		fin >> a[i].price;
-		fin >> a[i].deadline;
-		fin >> a[i].dtime;
-		fin >> a[i].address;
-		fin >> a[i].courierid;
-		if(a[i].courierid==0){//считаем заказы без курьера
+		fin >> sido;
+		fin >> sprice ;
+		fin >> sdeadline;
+		fin >> sdtime;
+		fin >> saddress;
+		fin >> scourierid;
+		a[i].setido(sido);
+		a[i].setprice(sprice);
+		a[i].setdeadline(sdeadline);
+		a[i].setdtime(sdtime);
+		a[i].setaddress(saddress);
+		a[i].setcourierid(scourierid);
+		if(a[i].getcourierid()==0){//считаем заказы без курьера
 			nul++;
 		}	
 	}
@@ -36,30 +48,30 @@ void oselect(string courierfile, string orderfile)
 	int x;
 	for(i=0;i<k;i++){//сортируем заказы по возрастанию крайнего срока доставки, для более эффективного распределения
 		for(j=0;j<k-1;j++){//метод пузырька
-			if(a[j].deadline>a[j+1].deadline){
-				x=a[j].ido;
-				a[j].ido=a[j+1].ido;
-				a[j+1].ido=x;
+			if(a[j].getdeadline()>a[j+1].getdeadline()){
+				x=a[j].getido();
+				a[j].setido(a[j+1].getido());
+				a[j+1].setido(x);
 				
-				x=a[j].price;
-				a[j].price=a[j+1].price;
-				a[j+1].price=x;
+				x=a[j].getprice();
+				a[j].setprice(a[j+1].getprice());
+				a[j+1].setprice(x);
 				
-				x=a[j].deadline;
-				a[j].deadline=a[j+1].deadline;
-				a[j+1].deadline=x;
+				x=a[j].getdeadline();
+				a[j].setdeadline(a[j+1].getdeadline());
+				a[j+1].setdeadline(x);
 				
-				x=a[j].dtime;
-				a[j].dtime=a[j+1].dtime;
-				a[j+1].dtime=x;
+				x=a[j].getdtime();
+				a[j].setdtime(a[j+1].getdtime());
+				a[j+1].setdtime(x);
 				
-				s=a[j].address;
-				a[j].address=a[j+1].address;
-				a[j+1].address=s;
+				s=a[j].getaddress();
+				a[j].setaddress(a[j+1].getaddress());
+				a[j+1].setaddress(s);
 				
-				x=a[j].courierid;
-				a[j].courierid=a[j+1].courierid;
-				a[j+1].courierid=x;
+				x=a[j].getcourierid();
+				a[j].setcourierid(a[j+1].getcourierid());
+				a[j+1].setcourierid(x);
 			}
 		}
 	}	
@@ -75,30 +87,44 @@ void oselect(string courierfile, string orderfile)
 	Courier *b=new Courier[j+1];//создаем массив курьеров
 	fin.close();
 	fin.open(courierfile);
+	int sidc;	
+	string sname;
+	string sphone;
+	int sorderid1;
+	int sorderid2;
+	int scar; 
+	int sdeliverytime;
 	for (i=0; i<j; i++){
-		fin >> b[i].idc;//считываем массив из файла курьеров
-		fin >> b[i].name ;
-		fin >> b[i].phone;
-		fin >> b[i].orderid1;
-		fin >> b[i].orderid2;
-		fin >> b[i].car;
-		fin >> b[i].deliverytime;
+		fin >> sidc;
+		fin >> sname ;
+		fin >> sphone;
+		fin >> sorderid1;
+		fin >> sorderid2;
+		fin >> scar;
+		fin >> sdeliverytime;
+		b[i].setidc(sidc);
+		b[i].setname(sname);
+		b[i].setphone(sphone);
+		b[i].setorderid1(sorderid1);
+		b[i].setorderid2(sorderid2);
+		b[i].setcar(scar);
+		b[i].setdeliverytime(sdeliverytime);
 	}
 	int tmp=0;
 	i=0;
 	int f;
 	int m=0;
 	int t=0;
-	for(i=0;i<k;i++){,,идем по массиву заказов
-		if(a[i].courierid==0){//ищем заказы без курьера
+	for(i=0;i<k;i++){//идем по массиву заказов
+		if(a[i].getcourierid()==0){//ищем заказы без курьера
 			f=0;//флаг
 			for (m=0;m<j;m++){//идем по массиву курьеров
-			 	if((f==0)&&(b[m].orderid1==0)&&(a[i].deadline>30-20*b[m].car)){//если этот заказ еще не присвоили, курьер свободен и достаточно быстрый 
-			 		b[m].orderid1=a[i].ido;//присваиваем соответствующие id
-				 	a[i].courierid=b[m].idc;
+			 	if((f==0)&&(b[m].getorderid1()==0)&&(a[i].getdeadline()>30-20*b[m].getcar())){//если этот заказ еще не присвоили, курьер свободен и достаточно быстрый 
+			 		b[m].setorderid1(a[i].getido());//присваиваем соответствующие id
+				 	a[i].setcourierid(b[m].getidc());
 			 		tmp++;//считаем распределенные заказы
-			 		b[m].deliverytime=30-20*b[m].car;//теперь число не нулевое
-			 		a[i].dtime=b[m].deliverytime;
+			 		b[m].setdeliverytime(30-20*b[m].getcar());//теперь число не нулевое
+			 		a[i].setdtime(b[m].getdeliverytime());
 			 		f=1;//флаг, теперь этот заказ назначен
 				}	
 			}
@@ -106,13 +132,13 @@ void oselect(string courierfile, string orderfile)
 	}
 	if(tmp<nul){//если еще остались нераспределенные заказы
 		for(i=0;i<k;i++){//идем по заказам	
-			if(a[i].courierid==0)	{//ищем свободные	
+			if(a[i].getcourierid()==0)	{//ищем свободные	
 				f=0;//флаг
 				for (m=0;m<j;m++){//идем по курьерам	
-				 	if((f==0)&&(b[m].orderid2==0)&&(a[i].deadline>b[m].deliverytime+30-b[m].car*20)){//если заказ еще не присвоили, и незанятый курьер успеет после выполнения своего текущего заказа
-				 		b[m].orderid2=a[i].ido;//присваиваем нужные id
-					 	a[i].courierid=b[m].idc;
-					 	a[i].dtime=b[m].deliverytime+30-20*b[m].car;//время доставки этого заказа
+				 	if((f==0)&&(b[m].getorderid2()==0)&&(a[i].getdeadline()>b[m].getdeliverytime()+30-b[m].getcar()*20)){//если заказ еще не присвоили, и незанятый курьер успеет после выполнения своего текущего заказа
+				 		b[m].setorderid2(a[i].getido());//присваиваем нужные id
+					 	a[i].setcourierid(b[m].getidc());
+					 	a[i].setdtime(b[m].getdeliverytime()+30-20*b[m].getcar());//время доставки этого заказа
 				 		f=1;//флаг, теперь заказ назначен
 				 		tmp++;
 					}	
@@ -123,16 +149,15 @@ void oselect(string courierfile, string orderfile)
 	fin.close();
 	fout.open(courierfile, ios::out);//перезаписываем курьеров
 	for (i=0; i<j; i++){
-		fout << b[i].idc <<" "<<b[i].name<<" "<<b[i].phone<<" "<<b[i].orderid1<<" "<<b[i].orderid2<<" "<<b[i].car<<" "<<b[i].deliverytime<<endl;
+		fout << b[i].getidc() <<" "<<b[i].getname()<<" "<<b[i].getphone()<<" "<<b[i].getorderid1()<<" "<<b[i].getorderid2()<<" "<<b[i].getcar()<<" "<<b[i].getdeliverytime()<<endl;
 	}
 	fout.close();
 	delete [] b;
 	fout.open(orderfile, ios::out);//перезаписываем заказы
 	for (i=0; i<k; i++){
-		fout << a[i].ido <<" "<<a[i].price<<" "<<a[i].deadline<<" "<<a[i].dtime<<" "<<a[i].address<<" "<<a[i].courierid<<endl;
+		fout << a[i].getido() <<" "<<a[i].getprice()<<" "<<a[i].getdeadline()<<" "<<a[i].getdtime()<<" "<<a[i].getaddress()<<" "<<a[i].getcourierid()<<endl;
 	}
 	fout.close();
 	delete [] a;	//уборка
 					
 }
-
