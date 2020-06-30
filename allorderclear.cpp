96e647c813b400701b0cd3124fecfb1d1b4631a1
IshuDivan <62930631+IshuDivan@users.noreply.github.com>
2020-06-30 22:08:56 +0300

    Update allorderclear.cpp
    Anotherone

--- allorderclear.cpp
+++ allorderclear.cpp
@@ -19,22 +19,36 @@ void allorderclear(string courierfile, string orderfile){
 		k++;
 	}
 	k--;
+	int sidc;	
+	string sname;
+	string sphone;
+	int sorderid1;
+	int sorderid2;
+	int scar; 
+	int sdeliverytime;		
+	Courier *a = new Courier[k+1];
 	fin.close();
-	fin.open(courierfile);
-	Courier *a = new Courier[k+1];//создаем массив объектов
+	fin.open(courierfile);	
 	for (i=0; i<k; i++){
-		fin >> a[i].idc;//считываем данные в массив
-		fin >> a[i].name ;
-		fin >> a[i].phone;
-		fin >> a[i].orderid1;
-		fin >> a[i].orderid2;
-		fin >> a[i].car;
-		fin >> a[i].deliverytime;
+		fin >> sidc;
+		fin >> sname ;
+		fin >> sphone;
+		fin >> sorderid1;
+		fin >> sorderid2;
+		fin >> scar;
+		fin >> sdeliverytime;
+		a[i].setidc(sidc);
+		a[i].setname(sname);
+		a[i].setphone(sphone);
+		a[i].setorderid1(sorderid1);
+		a[i].setorderid2(sorderid2);
+		a[i].setcar(scar);
+		a[i].setdeliverytime(sdeliverytime);
 	}
 	fin.close();
 	fout.open(courierfile,ios::out);
 	for (i=0; i<k; i++){ //перезаписываем данные курьеров обратно, но с id заказов и временем доставки =0
-		fout << a[i].idc<<" " <<a[i].name <<" " << a[i].phone <<" " << 0<<" " << 0 <<" " << a[i].car<<" " << 0<<endl;
+		fout << a[i].getidc() <<" "<<a[i].getname()<<" "<<a[i].getphone()<<" "<<0<<" "<<0<<" "<<a[i].getcar()<<" "<<0<<endl;
 	}
 	fout.close();
 	delete[]a;
