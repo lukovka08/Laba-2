717e0010080e7bf87983da19a97253079366335d
rozybaeva6 <61972638+rozybaeva6@users.noreply.github.com>
2020-06-30 21:23:32 +0300

    Update classes.h

--- classes.h
+++ classes.h
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+
 using namespace std;
+
 /*!     
           \brief Класс для работы с заказами
 */
+
 class Order {
 private:
 	int ido; ///< id заказа (идентификатор заказа)
@@ -15,40 +18,32 @@ private:
         int courierid; ///< id сотрудника (индентификатор сотрудника)
         int dtime; ///< время доставки заказа (число часов)
 public:
-	/*!   
-	       \arg courierfile Файл с курьерами
-		   \arg orderfile Файл с заказами
-    */
-	friend void allorderclear(string courierfile, string orderfile); // очистить файл заказов
-	/*!
-	     \arg orderfile Файл с заказами
-	*/
-	friend void allorderwrite(string orderfile); // вывести список заказов
-	/*!
-	      \arg courierfile Файл с курьерами
-		  \arg orderfile Файл с заказами
-	*/
-    friend void oselect(string courierfile, string orderfile); // распределение заказов по курьерам
-	/*!
-	       \arg orderfile Файл с заказами
-	*/
-	friend void addorder (string orderfile); // добавить заказ
-	/*!
-	        \arg orderfile Файл с заказами
-	*/
-	friend void osearchid(string orderfile); // поиск по id
-	/*!
-	         \arg courierfile Файл с курьерами
-			 \arg orderfile Файл с заказами
-	*/
-	friend void runtime(string courierfile, string orderfile); // промотать время вперед на n минут
+	int getido() { return ido; } // геттер для ido
+	void setido(int sido) { ido = sido; } // сеттер для ido
+ 
+	int getprice() { return price; } // геттер для price
+	void setprice(int sprice) { price = sprice; } // сеттер для price
+ 
+	int getdeadline() { return deadline; } // геттер для deadline
+	void setdeadline(int sdeadline) { deadline = sdeadline; } // сеттер для deadline
+ 
+	string getaddress() { return address; } // геттер для address
+	void setaddress(string saddress) { address = saddress; } // сеттер для address
+ 
+	int getcourierid() { return courierid; } // геттер для courierid
+	void setcourierid(int scourierid) { courierid = scourierid; } // сеттер для courierid
+ 
+	int getdtime() { return dtime; } // геттер для dtime
+	void setdtime(int sdtime) { dtime = sdtime; } // сеттер для dtime
 };
+
 /*!
            \brief Класс для работы с курьерами
 */
+
 class Courier {
-private: 
-	int idc; ///<  id сотрудника (идентификатор сотрудника)
+private:
+	int idc; ///<  id сотрудника (идентификатор сотрудника)	
 	string name; ///< имя сотрудника ( имя )
 	string phone; ///< телефон сотрудника (номер телефона)
 	int orderid1; ///< текущий заказ (есть, нет)
@@ -56,46 +51,26 @@ private:
 	int car; ///< машина для доставки (есть, нет)
 	int deliverytime; ///< время через которое курьер освободится (число часов)
 public:
-	/*!
-	         \arg courierfile Файл с курьерами
-			 \arg orderfile Файл с заказами
-	*/
-	friend void runtime(string courierfile, string orderfile); // промотать время вперед на n минут
-	/*!
-	         \arg courierfile Файл с курьерами
-	*/
-	friend void csearchid(string courierfile); // найти курьера по id
-	/*!
-	          \arg courierfile Файл с курьерами
-	*/
-	friend void addcourier(string courierfile); // добавить курьера
-	/*!
-	          \arg courierfile Файл с курьерами
-			  \arg orderfile Файл с заказами
-	*/
-	friend void courierdeleting(string courierfile, string orderfile); // удалить курьера
-	/*!
-	            \arg courierfile Файл с курьерами
-	*/
-	friend void csearchname(string courierfile); // найти курьера по имени
-	/*!
-				\arg courierfile Файл с курьерами
-	*/
-	friend void csearchphone(string courierfile); // найти курьера по телефону
-	/*!
-			  \arg courierfile Файл с курьерами
-			  \arg orderfile Файл с заказами
-	*/
-	friend void allcourierclear(string courierfile, string orderfile); // очистить список курьеров
-	/*!
-				\arg namefile Файл с курьерами
-	*/
-	friend void allcourierwrite(string namefile); // вывести список курьеров
-	/*!
-			  \arg courierfile Файл с курьерами
-			  \arg orderfile Файл с заказами
-	*/
-	friend void oselect(string courierfile, string orderfile); // распределение заказов по курьерам
+	int getidc() { return idc; } // геттер для idc
+	void setidc(int sidc) { idc = sidc; } // сеттер для idc
+
+	string getname() { return name; } // геттер для name
+	void setname(string sname) { name = sname; } // сеттер для name
+ 
+	string getphone() { return phone; } // геттер для phone
+	void setphone(string sphone) { phone = sphone; } // сеттер для phone
+ 
+	int getorderid1() { return orderid1; } // геттер для orderid1
+	void setorderid1(int sorderid1) { orderid1= sorderid1; } // сеттер для orderid1
+ 
+	int getorderid2() { return orderid2; } // геттер для orderid2
+	void setorderid2(int sorderid2) { orderid2 = sorderid2; } // сеттер для address
+ 
+	int getcar() { return car; } // геттер для car
+	void setcar(int scar) { car = scar; } // сеттер для car
+ 
+	int getdeliverytime() { return deliverytime; } // геттер для deliverytime
+	void setdeliverytime(int sdeliverytime) { deliverytime = sdeliverytime; } // сеттер для deliverytime
 };
 void allorderwrite(string orderfile);
 void allorderclear(string courierfile, string orderfile);
