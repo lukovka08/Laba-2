e282ed90c6abe0b71fe5859ff5d4183801af3efa
IshuDivan <62930631+IshuDivan@users.noreply.github.com>
2020-07-02 16:04:24 +0300

    Update oselect.cpp
    its Courier method, because we can

--- oselect.cpp
+++ oselect.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include "classes.h"
 using namespace std;
-void oselect(string courierfile, string orderfile)
+void Courier::oselect(string courierfile, string orderfile)
 { 
 	ifstream fin;
 	ofstream fout;	
